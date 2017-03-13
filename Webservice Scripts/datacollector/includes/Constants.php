76ceb62dd9b708ea5ae2c74c210d7aa79d539a57
newfotune <chiemeziemnwoke@yahoo.com>
2017-03-12 22:21:07 -0700

    Removed my private database information

--- Webservice Scripts/datacollector/includes/Constants.php
+++ Webservice Scripts/datacollector/includes/Constants.php
@@ -1,5 +1,5 @@
 <?php
-    define('DB_NAME','heroku_7d9ca065eba0788');
-    define('DB_USER','b70a60223efc33');
-    define('DB_PASSWORD','c8c02d5d');
-    define('DB_HOST','us-cdbr-iron-east-04.cleardb.net');
+    define('DB_NAME',[Database name]);
+    define('DB_USER',[DB username]);
+    define('DB_PASSWORD', [DB password]);
+    define('DB_HOST',[DB host]);
